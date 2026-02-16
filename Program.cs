5a9ab0990fbd6f52112e50f461596194904bfe97
rbjunk <118035460+rbjunk@users.noreply.github.com>
2026-02-16 14:23:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CPL_SCL_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
