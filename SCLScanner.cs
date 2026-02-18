ae4a10978953a18a4e8487b497049107dd3c129a
rbjunk <118035460+rbjunk@users.noreply.github.com>
2026-02-18 14:31:49 -0500

    Working Implementation. Add test case files.

---
+++ SCLScanner.cs
@@ -0,0 +1,146 @@
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
+namespace CPL_SCL_Project
+{
+    internal class SCLScanner
+    {
+        static void Main(string[] args)
+        {
+            string file_location = args[0];
+            Console.WriteLine("TEST");
+            tokenize(file_location);
+        }
+        public static List<Token> tokenize(string file_location)
+        {
+
+            List<Token> all_tokens = new List<Token>();
+            int current_identifier_id = 1000;
+            Dictionary<string, int> current_identifiers = new Dictionary<string, int>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(file_location))
+                {
+                    List<TokenDefinition> token_regex_patterns = new List<TokenDefinition>
+                    {
+                        new TokenDefinition(token_types.comment, @"//.*"),
+                        new TokenDefinition(token_types.comment, @"/\*.*?\*/"),
+                        new TokenDefinition(token_types.commentStart, @"/\*.*"),
+                        new TokenDefinition(token_types.literal, @"""[^""]*"""), //string literal
+                        new TokenDefinition(token_types.operators, @"[+\-*/^><=()]"), //operator check
+                        new TokenDefinition(token_types.keyword, @"\b(import|implementations|function|main|return|type|integer|double|char|num|is|variables|define|of|begin|display|set|exit|endfun|symbol|end|input|structures|pointer|head|last|NULL|ChNode|using|reverse|while|endwhile|call|constants|float|array|for|to|do|endfor)\b"), //keyword check
+                        new TokenDefinition(token_types.literal, @"\d+\.\d+"), //double check
+                        new TokenDefinition(token_types.literal, @"\d+"), //integer check
+                        new TokenDefinition(token_types.specialSymbols, @"[,\.]"), //special character check
+                        new TokenDefinition(token_types.identifier, @"[a-zA-Z_][a-zA-Z0-9_]*") //identifier check
+                    };
+                    string remaining_line_text;
+                    bool in_comment_block = false;
+                    while (!reader.EndOfStream)
+                    {
+                        remaining_line_text = reader.ReadLine();
+                        remaining_line_text = remaining_line_text.Trim();
+                        if (in_comment_block )
+                        {
+                            // Check if this line closes the block
+                            int closeIndex = remaining_line_text.IndexOf("*/");
+
+                            if (closeIndex != -1)
+                            {
+                                // Found the closing "*/"
+                                // Resume tokenizing AFTER the "*/" (add 2 for the length of */)
+                                remaining_line_text = remaining_line_text.Substring(closeIndex + 2).TrimStart();
+                                in_comment_block = false;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    
+                        while (!string.IsNullOrEmpty(remaining_line_text))
+                        {
+                            bool match_found = false;
+                            foreach (var pattern in token_regex_patterns)
+                            {
+                                var match = pattern.regex.Match(remaining_line_text);
+                                if(match.Success)
+                                {
+                                    if (pattern.type == token_types.comment)
+                                    {
+                                    }
+                                    else if (pattern.type == token_types.commentStart)
+                                    {
+                                        in_comment_block = true;
+                                        remaining_line_text = "";
+                                    }
+                                    else if (pattern.type == token_types.keyword)
+                                    {
+                                        if (TokenTypes.TokenList.TryGetValue(pattern.type, out var dict) &&
+                                            dict.TryGetValue(match.Value, out int code))
+                                        {
+                                            Console.WriteLine($"Token created: {pattern.type} id:{code} value:{match.Value}");
+                                        }
+                                    }
+                                    else if (pattern.type == token_types.operators)
+                                    {
+                                        if (TokenTypes.TokenList.TryGetValue(pattern.type, out var dict) &&
+                                            dict.TryGetValue(match.Value, out int code))
+                                        {
+                                            Console.WriteLine($"Token created: {pattern.type} id:{code} value:{match.Value}");
+                                        }
+                                    }
+                                    else if (pattern.type == token_types.identifier)
+                                    {
+                                        if (current_identifiers.ContainsKey(match.Value))
+                                        {
+                                            int identifier_value = current_identifiers[match.Value];
+                                            Console.WriteLine($"Token created: {pattern.type} id:{identifier_value} value:{match.Value}");
+                                        }
+                                        else
+                                        {
+                                            int identifier_value = current_identifier_id;
+                                            Console.WriteLine($"Token created: {pattern.type} id:{identifier_value} value:{match.Value}");
+                                            current_identifiers.Add(match.Value, identifier_value);
+                                            current_identifier_id++;
+                                        }
+                                    }
+                                    else if (pattern.type == token_types.specialSymbols)
+                                    {
+                                        if (TokenTypes.TokenList.TryGetValue(pattern.type, out var dict) &&
+                                            dict.TryGetValue(match.Value, out int code))
+                                        {
+                                            Console.WriteLine($"Token created: {pattern.type} id:{code} value:{match.Value}");
+                                        }
+                                    }
+                                    else if (pattern.type == token_types.literal)
+                                    {
+
+                                            Console.WriteLine($"Token created: {pattern.type} id:{600} value:{match.Value}");
+                                    }
+
+                                    if (!in_comment_block)
+                                    {
+                                        remaining_line_text = remaining_line_text.Substring(match.Length).TrimStart();
+                                    }
+                                    match_found = true;
+                                    break;
+                                }
+                            }
+                            if (!match_found)
+                            {
+                                Console.WriteLine(token_types.unknown + " " + remaining_line_text[0].ToString());
+                                remaining_line_text = remaining_line_text.Substring(1).TrimStart();
+                            }
+                        }
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.Message);
+            }
+            return all_tokens;
+        }
+    }
+}
---
+++ Tokens.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CPL_SCL_Project
+{
+    public enum token_types
+    {
+        comment,
+        commentStart,
+        literal,
+        keyword,
+        operators,
+        specialSymbols,
+        identifier,
+        endOfStatement,
+        unknown
+    }
+
+    public class TokenDefinition
+    {
+        public token_types type { get; set; }
+        public Regex regex { get; set; }
+        
+        public TokenDefinition(token_types type, string pattern)
+        {
+            this.type = type;
+            regex = new Regex("^" + pattern);
+        }
+    }
+    class TokenTypes
+    {
+        public static Dictionary<token_types, Dictionary<string, int>> TokenList = new Dictionary<token_types, Dictionary<string, int>>()
+        {
+            {
+                token_types.keyword, new Dictionary<string, int>()
+                {
+                    { "import", 0 },
+                    { "implementations", 1 },
+                    { "function", 2 },
+                    { "main", 3 },
+                    { "return", 4 },
+                    { "type", 5 },
+                    { "integer", 6 },
+                    { "double", 7 },
+                    { "char", 8 },
+                    { "num", 9 },
+                    { "is", 10 },
+                    { "variables", 11 },
+                    { "define", 12 },
+                    { "of", 13 },
+                    { "begin", 14 },
+                    { "display", 15 },
+                    { "set", 16 },
+                    { "exit", 17 },
+                    { "endfun", 18 },
+                    { "symbol", 19 },
+                    { "end", 20 },
+                    { "input", 21 },
+                    { "structures", 22 },
+                    { "pointer", 23 },
+                    { "head", 24 },
+                    { "last", 25 },
+                    { "NULL", 26 },
+                    { "ChNode", 27 },
+                    { "using", 28 },
+                    { "reverse", 29 },
+                    { "while", 30 },
+                    { "endwhile", 31 },
+                    { "call", 32 },
+                    { "constants", 33 },
+                    { "float", 34 },
+                    { "array", 35 },
+                    { "for", 36 },
+                    { "to", 37 },
+                    { "do", 38 },
+                    { "endfor", 39 },
+                }
+            },
+
+            {
+                token_types.operators, new Dictionary<string, int>()
+                {
+                    { "+", 400 },
+                    { "-", 401 },
+                    { "*", 402 },
+                    { "/", 403 },
+                    { "^", 404 },
+                    { ">", 405 },
+                    { "<", 406 },
+                    { "=", 407 },
+                    { "(", 408 },
+                    { ")", 409 },
+                }
+            },
+
+            { 
+                token_types.literal, new Dictionary<string, int>()
+                {
+                    { "literal", 500 }
+                }
+
+            },
+
+            {
+                token_types.specialSymbols, new Dictionary<string, int>()
+                {
+                    { ",", 800 },
+                    { ".", 801 }
+                }
+            }
+        };
+    }
+
+    class Token
+    {
+        public token_types token_type { get; set; }
+        public int id { get; set; }
+        public string value { get; set; }
+
+        public Token(token_types type, int id, string value)
+        {
+            token_type = type;
+            this.id = id;
+            this.value = value;
+        }
+    }
+}
